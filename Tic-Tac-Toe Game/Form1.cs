35d9d8c26d4f5847e3a6b52b15c4b07f5c77164b
Marvin James A. Erosa <mjae0109@gmail.com>
2022-05-23 21:42:36 +0800

    drawFunction

--- Tic-Tac-Toe Game/Form1.cs
+++ Tic-Tac-Toe Game/Form1.cs
@@ -10,7 +10,9 @@ namespace Tic_Tac_Toe_Game
         int R1C2VAL = 10;
         int R1C3VAL = 10;
         int R2C1VAL = 10;
+        int R2C2VAL = 10;
         int R3C1VAL = 10;
+        int R3C3VAL = 10;
         public gameScreen()
         {
             InitializeComponent();
@@ -166,13 +168,46 @@ namespace Tic_Tac_Toe_Game
 
         private void R2C2_Click(object sender, EventArgs e)
         {
+            /* FOR X AND O BUTTON */
             num = num + 1;
             int mod = num % 2;
+
             if (mod == 0)
+            {   /* FOR X AND O BUTTON */
                 R2C2.Text = "X";
+                /* FOR WIN SCENARIO */
+                R1C2VAL = 1;
+            }
             else
+            {   /* FOR X AND O BUTTON*/
                 R2C2.Text = "O";
+                /* FOR WIN SCENARIO*/
+                R2C2VAL = 0;
+            }
+
+            /* BUTTON DISABLE */
             R2C2.Enabled = false;
+
+            /* O WIN SCENARIO */
+            if (R1C1VAL == 0 & R2C2VAL == 0 & R3C3VAL == 0)
+            {
+                gameSatus.Text = "Player 1 Wins :)";
+                R1C1.Enabled = false;
+                R1C2.Enabled = false;
+                R1C3.Enabled = false;
+                R2C1.Enabled = false;
+                R2C3.Enabled = false;
+                R3C1.Enabled = false;
+                R3C2.Enabled = false;
+                R3C3.Enabled = false;
+            }
+
+            /*DRAW SCENARIO*/
+            if (num > 8)
+            {
+                gameSatus.Text = "Nobody wins :(";
+            }
+
         }
 
         private void R2C3_Click(object sender, EventArgs e)
@@ -236,13 +271,39 @@ namespace Tic_Tac_Toe_Game
 
         private void R3C3_Click(object sender, EventArgs e)
         {
+            /* FOR X AND O BUTTON */
             num = num + 1;
             int mod = num % 2;
+
             if (mod == 0)
+            {   /* FOR X AND O BUTTON */
                 R3C3.Text = "X";
+                /* FOR WIN SCENARIO */
+                R3C3VAL = 1;
+            }
             else
+            {   /* FOR X AND O BUTTON*/
                 R3C3.Text = "O";
+                /* FOR WIN SCENARIO*/
+                R3C3VAL = 0;
+            }
+
+            /* BUTTON DISABLE */
             R3C3.Enabled = false;
+
+            /* O WIN SCENARIO */
+            if (R1C1VAL == 0 & R2C2VAL == 0 & R3C3VAL == 0)
+            {
+                gameSatus.Text = "Player 1 Wins :)";
+                R1C1.Enabled = false;
+                R1C2.Enabled = false;
+                R1C3.Enabled = false;
+                R1C1.Enabled = false;
+                R2C2.Enabled = false;
+                R2C3.Enabled = false;
+                R3C1.Enabled = false;
+                R3C2.Enabled = false;
+            }
         }
     }
 }
